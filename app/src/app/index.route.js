c7efbd9a07655f15e73fa55292097c5dc8c971cb
filmse <filmpurelove@gmail.com>
2016-05-15 18:36:49 +0700

    link controller client-side finished

--- app/src/app/index.route.js
+++ app/src/app/index.route.js
@@ -12,6 +12,26 @@
         controller: 'MainController',
         controllerAs: 'main'
       })
+      .when('/product', {
+        templateUrl: 'app/product/product.html',
+        controller: 'productController'
+      })
+      .when('/shop', {
+        templateUrl: 'app/shop/shop.html',
+        controller: 'shopController'
+      })
+      .when('/event', {
+        templateUrl: 'app/event/event.html',
+        controller: 'eventController'
+      })
+      .when('/profile', {
+        templateUrl: 'app/profile/profile.html',
+        controller: 'profileController'
+      })
+      .when('/register', {
+        templateUrl: 'app/register/register.html',
+        controller: 'registerController'
+      })
       .otherwise({
         redirectTo: '/'
       });
---
+++ app/src/app/profile/profileController.js
@@ -0,0 +1,15 @@
+/**
+ * Created by film on 5/15/2016.
+ */
+(function() {
+  'use strict';
+
+  angular
+    .module('app')
+    .controller('ProfileController', profileController);
+
+  /** @ngInject */
+  function profileController() {
+
+  }
+})();
